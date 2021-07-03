fe1e3f869eb2dc45986644990dac753ee2bebbea
zolfaShefreie <z.shefreie@gmail.com>
2021-07-03 20:56:03 +0430

    complete signup error handleing

--- src/components/signup.js
+++ src/components/signup.js
@@ -12,14 +12,22 @@ class SignUp extends React.Component{
             backgroundColor: '#3b3636',
             color: 'antiquewhite'
         }
+        this.error_style={
+            color: 'red',
+            fontWeight: 'normal',
+            fontSize: '.6em',
+        }
         this.state = {
+            'errors': {},
+            'password_error': '',
             'password': '',
             'username': '',
             'lastname': '',
             'firstname': '',
-            'password-repeat': '',
+            'password_repeat': '',
             'email': '',
             'user_type': 'normal',
+
         }
         this.handleChange = this.handleChange.bind(this);
         this.handleSubmit = this.handleSubmit.bind(this);
@@ -29,6 +37,12 @@ class SignUp extends React.Component{
         return (
             <div className="container box" style={{width: '42%'}}>
                 <h1 style={{color: '#3b3636'}}>SignUp</h1>
+                {
+                    'non_field_errors' in this.state.errors &&
+                    <div className='alert alert-danger mt-3'>
+                        <p className="text-center">{this.state.errors['non_field_errors']}</p>
+                    </div>
+                }
                 <form className="form container" style={{width: '98%'}}>
                     <div className="form-row mr-4 ml-4">
                         <div className="col">
@@ -37,6 +51,12 @@ class SignUp extends React.Component{
                                        className="col-form-label col-form-label-sm">FirstName</label>
                                 <input type="text" className="form-control" style={this.input_style} id="firstname"
                                        onChange={this.handleChange} required/>
+                                {
+                                    'first_name' in this.state.errors &&
+                                    <div style={this.error_style}>
+                                        {this.state.errors['first_name']}
+                                    </div>
+                                }
                             </div>
 
                         </div>
@@ -46,6 +66,12 @@ class SignUp extends React.Component{
                                        className="col-form-label col-form-label-sm">LastName</label>
                                 <input type="text" className="form-control" style={this.input_style} id="lastname"
                                        onChange={this.handleChange} required/>
+                                {
+                                    'last_name' in this.state.errors &&
+                                    <div style={this.error_style}>
+                                        {this.state.errors['last_name']}
+                                    </div>
+                                }
                             </div>
                         </div>
                     </div>
@@ -53,24 +79,45 @@ class SignUp extends React.Component{
                         <label htmlFor="email" className="col-form-label col-form-label-sm">Email</label>
                         <input type="text" className="form-control" id="email" style={this.input_style}
                                placeholder="user@example.com" onChange={this.handleChange} required/>
+                        {
+                            'email' in this.state.errors &&
+                            <div style={this.error_style}>
+                                {this.state.errors['email']}
+                            </div>
+                        }
                     </div>
                     <div className="form-group row m-2">
                         <label htmlFor="username" className="col-form-label col-form-label-sm">Username</label>
                         <input type="text" className="form-control" id="username" style={this.input_style}
                                onChange={this.handleChange} required/>
+                        {
+                            'username' in this.state.errors &&
+                            <div style={this.error_style}>
+                                {this.state.errors['username']}
+                            </div>
+                        }
                     </div>
                     <div className="form-group row m-2">
                         <label htmlFor="password" className="col-form-label col-form-label-sm">Password</label>
                         <input type="password" className="form-control" id="password" style={this.input_style}
                                onChange={this.handleChange} required/>
+                        {
+                            'password' in this.state.errors &&
+                            <div style={this.error_style}>
+                                {this.state.errors['password']}
+                            </div>
+                        }
                     </div>
                     <div className="form-group row m-2">
-                        <label htmlFor="password-repeat" className="col-form-label col-form-label-sm">Repeat Password</label>
+                        <label htmlFor="password_repeat" className="col-form-label col-form-label-sm">Repeat Password</label>
                         <input type="password" className="form-control" onChange={this.handleChange}
-                               id="password-repeat" style={this.input_style} required/>
+                               id="password_repeat" style={this.input_style} required/>
+                        {
+                            <div style={this.error_style}>
+                                {this.state.password_error}
+                            </div>
+                        }
                     </div>
-
-
                     <div className="custom-control custom-radio custom-control-inline mt-2 mb-2">
                         <input type="radio" id="customRadioInline1" name="customRadioInline1"
                                className="custom-control-input" defaultChecked onChange={this.handleChange}/>
@@ -89,7 +136,6 @@ class SignUp extends React.Component{
     }
     async handleSubmit(event){
         event.preventDefault();
-        window.alert('hi')
 
         await axios.post(`${this.URL}`, {
             username: this.state.username,
@@ -97,25 +143,31 @@ class SignUp extends React.Component{
             first_name: this.state.firstname,
             last_name: this.state.lastname,
             email: this.state.email,
-            user_type: 'this.state.user_type'
+            user_type: this.state.user_type
         }).then((response) => {
             console.log(response.data);
         }, (error) => {
             console.log('error', Object.assign({}, error.response.data));
+            this.setState({errors: error.response.data})
         });
 
     }
 
-    handleChange(event){
+    async handleChange(event){
+        await this.setState({'password_error': ""})
         if (event.target.id === 'customRadioInline2' && event.target.value === 'on'){
-            this.setState({'user_type': 'podcast_producer'})
+            await this.setState({'user_type': 'podcast_producer'})
         }else if(event.target.id === 'customRadioInline1' && event.target.value === 'on'){
-            this.setState({'user_type': 'normal'})
+            await this.setState({'user_type': 'normal'})
         }
         else {
             const newState = {}
             newState[event.target.id] = event.target.value
-            this.setState(newState);
+            await this.setState(newState);
+        }
+
+        if (this.state.password !== this.state.password_repeat){
+            await this.setState({'password_error': "the password fields doesn't match to each other"})
         }
     }
 }
